2c5951b186c42062e7d9277946069dc96eadd1fd
Hedi van Meurs <alisoun@gmail.com>
2019-05-29 11:01:59 +0100

    simple checkout class

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,26 +1,14 @@
+require_relative 'items'
+
 class Checkout
   attr_accessor :total, :items
 
-  ITEMS = {
-    "A" => 30,
-    "B" => 20,
-    "C" => 60,
-    "D" => 15
-  }
 
   def initialize
     @total = 0
     @items = []
-    # @rules = rules
   end
 
-  # rules will be passed as a hash of hashes that has fixed keys for discounts
-  # these can be altered and added to
-  # def check_rules(@rules, @items)
-  #   rules["quantity"].each do |array, value|
-  #
-  #   end
-  # end
 
   def scan(item)
     @items << item
--- lib/interface.rb
+++ lib/interface.rb
@@ -1,16 +1,13 @@
 require_relative 'checkout'
 
 
-
-# co = Checkout.new({
-#   "quantity" => {
-#   ["A", "A", "A"] => 75,
-#   ["B", "B"] => 35},
-#   "price" => {150 => 20}
-# })
-
+# rules will be passed as a hash of hashes that has fixed keys for different
+# types of discounts, these can be altered and added to.
 co = Checkout.new
+
 co.scan("A")
 co.scan("B")
 price = co.total
+items = co.items
 puts price
+puts items
